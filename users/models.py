0455d1399c594860acae0737b829e9675958e6d6
Sarthak Kumar <43024535+ksarthak4ever@users.noreply.github.com>
2019-01-26 22:34:31 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
 
 		img = Image.open(self.image.path) #this will open the image of the current instance
 
-		if img.height > 300 or img.width >300: #to resize profile image
+		if img.height > 300 or img.width >300: #to resize profile image accordingly
 			output_size = (300,300)
 			img.thumbnail(output_size)
 			img.save(self.image.path)
